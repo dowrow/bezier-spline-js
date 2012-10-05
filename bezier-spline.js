39788341168618033d7266a237db58e404f8e045
Leszek Rybicki <leszek@rybicki.cc>
2012-10-05 16:49:39 +0900

    demo

--- bezier-spline.js
+++ bezier-spline.js
@@ -43,10 +43,12 @@ Spline = function(options){
 	this.stepLength = options.stepLength || 60;
 	this.length = this.points.length;
 	this.delay = 0;
+	// this is to ensure compatibility with the 2d version
+	for(var i=0; i<this.length; i++) this.points[i].z = this.points[i].z || 0;
 	for(var i=0; i<this.length-1; i++){
 		var p1 = this.points[i];
 		var p2 = this.points[i+1];
-		this.centers.push({"x":(p1.x+p2.x)/2, "y":(p1.y+p2.y)/2});
+		this.centers.push({x:(p1.x+p2.x)/2, y:(p1.y+p2.y)/2, z:(p1.z+p2.z)/2});
 	}
 	this.controls.push([this.points[0],this.points[0]]);
 	for(var i=0; i<this.centers.length-1; i++){
@@ -54,12 +56,15 @@ Spline = function(options){
 		var p2 = this.centers[i+1];
 		var dx = this.points[i+1].x-(this.centers[i].x+this.centers[i+1].x)/2;
 		var dy = this.points[i+1].y-(this.centers[i].y+this.centers[i+1].y)/2;
+		var dz = this.points[i+1].z-(this.centers[i].y+this.centers[i+1].z)/2;
 		this.controls.push([{
 			x:(1.0-this.sharpness)*this.points[i+1].x+this.sharpness*(this.centers[i].x+dx),
-			y:(1.0-this.sharpness)*this.points[i+1].y+this.sharpness*(this.centers[i].y+dy)},
+			y:(1.0-this.sharpness)*this.points[i+1].y+this.sharpness*(this.centers[i].y+dy),
+			z:(1.0-this.sharpness)*this.points[i+1].z+this.sharpness*(this.centers[i].z+dz)},
 		{
 			x:(1.0-this.sharpness)*this.points[i+1].x+this.sharpness*(this.centers[i+1].x+dx),
-			y:(1.0-this.sharpness)*this.points[i+1].y+this.sharpness*(this.centers[i+1].y+dy)}]);
+			y:(1.0-this.sharpness)*this.points[i+1].y+this.sharpness*(this.centers[i+1].y+dy),
+			z:(1.0-this.sharpness)*this.points[i+1].z+this.sharpness*(this.centers[i+1].z+dz)}]);
 	}
 	this.controls.push([this.points[this.length-1],this.points[this.length-1]]);
 	this.steps = this.cacheSteps(this.stepLength);
@@ -75,7 +80,7 @@ Spline.prototype.cacheSteps = function(mindist){
 	steps.push(0);
 	for(var t=0; t<this.duration; t+=10){
 		var step = this.pos(t);
-		var dist = Math.sqrt((step.x-laststep.x)*(step.x-laststep.x)+(step.y-laststep.y)*(step.y-laststep.y));
+		var dist = Math.sqrt((step.x-laststep.x)*(step.x-laststep.x)+(step.y-laststep.y)*(step.y-laststep.y)+(step.z-laststep.z)*(step.z-laststep.z));
 		if(dist>mindist){
 			steps.push(t);
 			laststep = step;
@@ -92,7 +97,7 @@ Spline.prototype.vector = function(t){
 	var p2 = this.pos(t-10);
 	return {
 		angle:180*Math.atan2(p1.y-p2.y, p1.x-p2.x)/3.14,
-		speed:Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y))
+		speed:Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y)+(p2.z-p1.z)*(p2.z-p1.z))
 	}
 }
 
@@ -156,6 +161,7 @@ Spline.prototype.pos = function(time){
 		var pos = {
 			x : p2.x * b[0] + c2.x * b[1] +c1.x * b[2] + p1.x * b[3],
 			y : p2.y * b[0] + c2.y * b[1] +c1.y * b[2] + p1.y * b[3]
+			z : p2.z * b[0] + c2.z * b[1] +c1.z * b[2] + p1.z * b[3]
 		}
 		return pos; 
 	}
